06e1cf98664c1c51836b2531680bbe6149eecd06
jmlee0527 <wjdalsskt@naver.com>
2023-08-04 18:42:32 +0900

    feat : 음성 인식 검색 기능 추가

--- js/home.js
+++ js/home.js
@@ -19,8 +19,8 @@ async function videoData(data) {
     try {
         // 로딩 이미지 생성 및 스타일 설정
         const loadingImage = document.createElement("img");
-        loadingImage.src = "./img/sidebar/spin.gif";
-        loadingImage.style.background = `url("./img/sidebar/spin.gif") no-repeat center`;
+        loadingImage.src = "./img/sidebar/loading.gif";
+        loadingImage.style.background = `url("./img/sidebar/loading.gif") no-repeat center`;
         loadingImage.style.backgroundSize = "contain";
         loadingImage.style.width = "100px";
         loadingImage.style.height = "100px";
@@ -261,4 +261,40 @@ if (searchText) {
         fetchData();
     }
 
+//음성 인식 검색 기능
+const searchConsole1 = document.getElementsByClassName("mic-icon");
 
+function availabilityFunc() {
+
+    recognition = new webkitSpeechRecognition() || new SpeechRecognition();
+    recognition.lang = "ko"; // 음성인식에 사용되고 반환될 언어
+    recognition.maxAlternatives = 5; 
+
+    if (!recognition) {
+    alert("현재 브라우저는 사용이 불가능합니다.");
+    }
+}
+//음성녹음을 실행하는 함수
+function startRecord() {
+    console.log("시작");
+
+    // 마이크 클릭 시 음성인식 시작
+    recognition.addEventListener("speechstart", () => {
+    console.log("인식");
+    });
+
+    //음성인식이 끝까지 이루어지면 중단
+    recognition.addEventListener("speechend", () => {
+    console.log("인식2");
+    });
+
+    //음성인식 결과를 반환
+    recognition.addEventListener("result", (e) => {
+    const transcript = e.results[0][0].transcript;
+    search(transcript);
+    });
+
+    recognition.start();
+}
+searchConsole1[0].addEventListener("click", startRecord);
+window.addEventListener("load", availabilityFunc);
